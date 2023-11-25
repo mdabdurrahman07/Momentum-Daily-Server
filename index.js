5ddeda9a0fcb2650dc15f7780bbc22cc30fe8a77
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-11-25 19:14:43 +0600

    server implemented and data server added(mongodb)

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+
+app.use(express.json())
+app.use(cors())
+
+// MomentumDaily
+// jaB6FHZmf4BdOEVF
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abtiefd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/' , async (req , res ) => {
+    res.send('Momentum Daily server is Running ')
+})
+app.listen(port , ()=> {
+    console.log(`This server is running on ${port}`)
+})
