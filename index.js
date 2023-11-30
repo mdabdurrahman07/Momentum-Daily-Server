d58b1b8727da03e4a314bb26046654c26637098c
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-11-30 23:31:56 +0600

    vercel issue

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // middlewares
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+
 
 // middlewares for JWT
 const verifyToken = (req , res, next) => {
